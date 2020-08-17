7f25c50cd5549c81e12d7aa81fc0c63070f68701
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-08-17 17:59:23 +0600

    Update Toolbox_Update_Info.bat

--- MagicX_Toolbox/Updater/Toolbox_Update_Info.bat
+++ MagicX_Toolbox/Updater/Toolbox_Update_Info.bat
@@ -1,3 +1,4 @@
-SET "Update_Version=1.5"
-SET "Update_FileName=ToolBox_Update.zip"
+SET "Update_Version=2.0.1"
+SET "Update_FileName=Toolbox_New.zip"
 SET "DNL_LINK=https://raw.githubusercontent.com/Ahsan400/MagicX_Mod_Files/master/MagicX_Toolbox/Updater"
+EXIT /B
