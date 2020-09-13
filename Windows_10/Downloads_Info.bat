8086a1fa8828d1f03c8fbb45690ecd0f0cf7fe25
Ahsan40 <30434801+Ahsan40@users.noreply.github.com>
2020-09-13 14:25:32 +0600

    [Update] IObit Uninstaller v10.0.2.21

--- Windows_10/Downloads_Info.bat
+++ Windows_10/Downloads_Info.bat
@@ -20,7 +20,7 @@ SET "CNTXT_OPT16=qBittorrent v4.2.5"
 SET "CNTXT_OPT17=qBittorrent Themes"
 SET "CNTXT_OPT18=TeamViewer v15.7.7.0"
 SET "CNTXT_OPT19=GOM Player Plus V2.3.56.5320"
-SET "CNTXT_OPT20=IObit Uninstaller v9.6.0.3"
+SET "CNTXT_OPT20=IObit Uninstaller v10.0.2.21"
 SET "CNTXT_OPT21=IObit Software Updater Pro v3.2.0.1751"
 
 
